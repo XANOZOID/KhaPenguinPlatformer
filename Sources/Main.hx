91ca5ded5370878aa8fe38112d83bc484239f4ac
xan <29788070+XANOZOID@users.noreply.github.com>
2020-08-17 01:45:21 -0700

    create sprites/animations classes

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -28,6 +28,91 @@ import format.tmx.Data.TmxMap;
 import format.tmx.Data.TmxTileset;
 import format.tmx.Reader;
 
+
+@:structInit class Sprite {
+	public final texture:Image;
+	public final frameX:Int;
+	public final frameY:Int;
+	public final frameW:Int;
+	public final frameH:Int;
+	public final originX:Float;
+	public final originY:Float;
+	
+	public function new(texture, frameX, frameY, frameW, frameH, originX:Float, originY:Float) {
+		this.texture = texture;
+		this.frameX = frameX;
+		this.frameY = frameY;
+		this.frameW = frameW;
+		this.frameH = frameH;
+		this.originX = originX * frameW;
+		this.originY = originY * frameH;
+	}
+
+	public function draw(g2:Graphics, x:Float, y:Float) {
+		g2.drawSubImage(texture, 
+			x - originX, y - originY, 
+			frameX, frameY, frameW, frameH);
+	}
+
+	public function drawScaled(g2:Graphics, x:Float, y:Float, scaleX:Float, scaleY:Float) {
+		g2.drawScaledSubImage(
+			texture, frameX, frameY, frameW, frameH,
+			x - originX * scaleX, y - originY * scaleY, 
+			frameW * scaleX, frameH * scaleY
+		);
+	}
+}
+
+@:structInit class Animation extends Sprite {
+	public final frames:Int;
+	public final columns:Int;
+	public final separationX:Int;
+	public final separationY:Int;
+	public var playSpeed:Float;
+	public var currentFrame:Float;
+	public function new(texture, frameX, frameY, frameW, frameH, frames, originX:Float, originY:Float, columns, separationX, separationY, playSpeed) {
+		super(texture, frameX, frameY, frameW, frameH, originX, originY);
+		this.frames = frames;
+		this.columns = columns;
+		this.playSpeed = playSpeed;
+		this.separationX = separationX;
+		this.separationY = separationY;
+	}
+
+	override public function draw(g2:Graphics, x:Float, y:Float) {
+		animate((drawFrame, xOff, yOff) -> {
+			g2.drawSubImage(texture, x - originX, y - originY, 
+				frameX + (frameW * xOff) + separationX * xOff, 
+				frameY + (frameH * yOff) + separationY * yOff, 
+				frameW, 
+				frameH
+			);
+		});
+	}
+	
+	override public function drawScaled(g2:Graphics, x:Float, y:Float, scaleX:Float, scaleY:Float) {
+		animate((drawFrame, xOff, yOff) -> {
+			g2.drawScaledSubImage(texture,
+				frameX + (frameW * xOff) + separationX * xOff, 
+				frameY + (frameH * yOff) + separationY * yOff, 
+				frameW,
+				frameH,
+				x - originX * scaleX, y - originY * scaleY,
+				frameW * scaleX,
+				frameH * scaleY
+			);
+		});
+	}
+
+	inline function animate(draw:(Int, Int, Int)->Void) {
+		final drawFrame = Math.floor(currentFrame);
+		final xOff = drawFrame % columns;
+		final yOff = Math.floor(drawFrame / columns);
+		draw(drawFrame, xOff, yOff);
+		currentFrame += playSpeed;
+		if (currentFrame > frames) currentFrame -= frames;
+	}
+}
 class Main {
 	static var logo = ["1 1 1 1 111", "11  111 111", "1 1 1 1 1 1"];
 
