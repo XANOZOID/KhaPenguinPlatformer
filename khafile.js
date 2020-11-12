6476f63c9c4f33e4f23f130cdbabaf623a4c37b2
xan <29788070+XANOZOID@users.noreply.github.com>
2020-11-12 10:23:52 -0700

    something something setup

--- khafile.js
+++ khafile.js
@@ -7,4 +7,8 @@ project.addAssets('assets/**', {
 project.addShaders('Shaders/**');
 project.addSources('Sources');
 project.addLibrary("haxe-format-tiled")
+
+project.addSources('overrides/haxe-format-tiled')  
+// too lazy to do much about this, but I just added a way (with a single line for both files) to get the hex-string instead of letting HFT taking care of it.
+
 resolve(project);
---
+++ readme.md
@@ -0,0 +1,5 @@
+Personal project . . . 
+
+
+- Please do not use the penguin art, the penguin character that is, in other projects. 
+- MPL license: https://www.mozilla.org/en-US/MPL/2.0/
