8996db57c19f53665a4f2261b1e1a64d95e1b31a
xan <29788070+XANOZOID@users.noreply.github.com>
2020-11-13 16:34:51 -0700

    why do I need to do this

--- maps/tset2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="asdasd" tilewidth="16" tileheight="16" spacing="1" tilecount="400" columns="20">
- <image source="monochrome_tilemap_transparent.png" width="339" height="339"/>
-</tileset>
